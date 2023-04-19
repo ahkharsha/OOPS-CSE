6ff6b40d9dd630e959f6b908e608870b615d8310
Harsha Kumar <ahkharsha@gmail.com>
2023-04-20 03:00:37 +0530

    Create Float_to_Int.c

---
+++ Float_to_Int.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float f = 3.14;
+    int i = (int)f;
+
+    printf("Floating point number: %f\n", f);
+    printf("Integer value: %d\n", i);
+
+    return 0;
+}
