82ca1d5ce258e50d5bccab0ebcdc225337b65a02
Harsha Kumar <ahkharsha@gmail.com>
2023-05-03 15:09:54 +0530

    Create CAT 1.c

---
+++ CAT 1.c
@@ -0,0 +1,30 @@
+//C program to find addition, subtraction, multiplication and division using pointers
+#include <stdio.h>
+
+void Addition(int *m, int *n);
+
+void Subtraction(int *m, int *n);
+
+void Multiplication(int *m, int *n);
+
+void Division(int *m, int *n);
+
+int main() {
+    int x,y;
+    scanf("%d",&x);
+    scanf("%d",&y);
+    
+    Addition(&x,&y);
+    
+    Subtraction(&x,&y);
+
+    return 0;
+}
+
+void Addition(int *m, int* n){
+    printf("%.1f\n",(*m+*n)/1.0);
+}
+
+void Subtraction(int *m, int* n){
+    printf("%.1f\n",(*m-*n)/1.0);
+}
