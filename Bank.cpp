67a02aad8f20fe5dcf1423a4c9bbfcd1711559a5
Harsha Kumar <ahkharsha@gmail.com>
2023-05-21 22:18:47 +0530

    Update Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -5,41 +5,40 @@ using namespace std;
 
 class BankAccount {
 private:
-    string accountNumber;
-    double balance;
+    double bal,accNo;
 
 public:
-    BankAccount(string accountNumber,double initialBalance = 0.0) {
-        this->accountNumber=accountNumber;
-        this->balance=initialBalance;
+    BankAccount(double accNo,double iniBal = 0.0) {
+        this->accNo=accNo;
+        this->bal=iniBal;
     }
 
-    void deposit(double amount) {
-        if (amount > 0) {
-            balance += amount;
-            cout<<"Deposited $"<<amount<<" into Account No. "<<accountNumber<<endl;
+    void deposit(double amt) {
+        if (amt>0) {
+            bal+=amt;
+            cout<<"Deposited $"<<amt<<" into Account No. "<<accNo<<endl;
         } else {
             cout<<"Invalid deposit amount. Amount must be positive."<<endl;
         }
     }
 
-    void withdraw(double amount) {
-        if (amount>0&&amount<=balance) {
-            balance-=amount;
-            cout<<"Withdrew $"<<amount<<" from account "<<accountNumber<< endl;
+    void withdraw(double amt) {
+        if ((amt>0)&(amt<=bal)) {
+            bal-=amt;
+            cout<<"Withdrew $"<<amt<<" from account "<<accNo<< endl;
         } else {
             cout<<"Insufficient funds or invalid withdrawal amount."<<endl;
         }
     }
 
     void checkBalance() {
-        cout<<"Account Number: "<<accountNumber<< "    Available balance: "<< balance<<endl<<endl;
+        cout<<"Account Number: "<<accNo<< "    Available balance: $"<< bal<<endl<<endl;
     }
 };
 
 int main() {
-    int iniBal,c;
-    string accNo;
+    double iniBal,accNo;
+    int c;
     cout<<"Enter the account number: "<<endl;
     cin>>accNo;
     cout<<"Enter the initial balance: "<<endl;
@@ -49,39 +48,38 @@ int main() {
     cout<<"Menu"<<endl<<"1. Check Account balance"<<endl<<"2. Deposit money"<<endl<<"3. Withdraw money"<<endl<<"4. Exit"<<endl<<"Enter your choice: ";
     cin>>c;
     
-    while ((c!=4)&(c>0)&(c<4)) {
+    while (c!=4) {
         switch(c) {
             case 1:
-            account.checkBalance();
-            break;
+                account.checkBalance();
+                break;
     
             case 2:
-            int dep;
-            cout<<"Enter the amount to deposit: "<<endl;
-            cin>>dep;
-            account.deposit(dep);
-            account.checkBalance();
-            break;
+                int dep;
+                cout<<"Enter the amount to deposit: "<<endl;
+                cin>>dep;
+                account.deposit(dep);
+                account.checkBalance();
+                break;
     
             case 3:
-            int wd;
-            cout<<"Enter the amount to withdraw"<<endl;
-            cin>>wd;
-            account.withdraw(wd);
-            account.checkBalance();
-            break;
+                int wd;
+                cout<<"Enter the amount to withdraw: "<<endl;
+                cin>>wd;
+                account.withdraw(wd);
+                account.checkBalance();
+                break;
+            
+            default:
+                cout<<"Invalid choice. Please try again."<<endl<<endl;
+                break;
         }
         
         cout<<"Menu"<<endl<<"1. Check Account balance"<<endl<<"2. Deposit money"<<endl<<"3. Withdraw money"<<endl<<"4. Exit"<<endl<<"Enter your choice: ";
         cin>>c;
     }
-    if ((c<1)||(c>4)) {
-        cout<<"Invalid choice. Exiting..."<<endl;
-    }
     
-    else {
-        cout<<"Exiting..."<<endl;
-    }
+    cout<<"Exiting..."<<endl;
 
     return 0;
 }
